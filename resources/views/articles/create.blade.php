0fde65fde403cd39b6cb1b9ef6d125cd5b2e01b2
midgjs <sjqksk.10@gmail.com>
2023-07-16 07:57:25 +0900

    글쓰기

---
+++ resources/views/articles/create.blade.php
@@ -0,0 +1,16 @@
+<html>
+    <head>
+
+    </head>
+    <body>
+        <form action="/articles" method="POST">
+            <h1>글쓰기</h1>
+
+            <input type="text">
+            <input type="button" value="저장하기">
+
+
+
+        </form>
+    </body>
+</html>
