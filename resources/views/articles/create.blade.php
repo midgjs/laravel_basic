cea040b7af9d6342d5c65d3cf2423c412057daf7
midgjs <sjqksk.10@gmail.com>
2023-07-18 22:23:00 +0900

    vite / tailwind 적용

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -2,15 +2,17 @@
     <head>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
-    <body class="bg-blue-300">
-        <form action="/articles" method="POST">
-            <h1>글쓰기</h1>
+    <body class="bg-red-200">
+    <form action="/articles" method="POST">
+        <div class="container p-5">
+        
+            <h1 class="text-2xl">글쓰기</h1>
 
-            <input type="text">
-            <input type="button" value="저장하기">
-
-
-
-        </form>
+            <input type="text" class="block w-full mb-2 mt-3 rounded">
+            <input type="button" value="저장하기" class="py-1 px-3 bg-black text-white rounded text-xs">
+        
+        
+        </div>
+    </form>
     </body>
 </html>
