3527f1d70d2d06ba5f41a97a7a671227126f5519
midgjs <sjqksk.10@gmail.com>
2023-07-19 07:00:17 +0900

    유효성 검증(request객체 사용)

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,7 +7,7 @@
         <div class="container p-5">
             <h1 class="text-2xl">글쓰기</h1>
             @csrf
-            <input type="text" class="block w-full mb-2 mt-3 rounded">
+            <input type="text" name="body" class="block w-full mb-2 mt-3 rounded">
             <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
         
         
