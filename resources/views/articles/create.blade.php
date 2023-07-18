a20dd0b68b535cce681127a9feace252fb58dbff
midgjs <sjqksk.10@gmail.com>
2023-07-19 06:48:22 +0900

    저장하기

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -5,11 +5,10 @@
     <body class="bg-red-200">
     <form action="/articles" method="POST">
         <div class="container p-5">
-        
             <h1 class="text-2xl">글쓰기</h1>
-
+            @csrf
             <input type="text" class="block w-full mb-2 mt-3 rounded">
-            <input type="button" value="저장하기" class="py-1 px-3 bg-black text-white rounded text-xs">
+            <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
         
         
         </div>
