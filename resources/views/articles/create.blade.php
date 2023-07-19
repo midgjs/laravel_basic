067766d25ee8ad8da95aeb666f80f2815696e6aa
midgjs <sjqksk.10@gmail.com>
2023-07-19 21:18:44 +0900

    유효성검증(에러메세지표시)

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -8,8 +8,12 @@
             <h1 class="text-2xl">글쓰기</h1>
             @csrf
             <input type="text" name="body" class="block w-full mb-2 mt-3 rounded">
+            @error('body')
+                <p class="text-xs text-red-500 mb-3"> {{ $message }} </p>
+            @enderror
             <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
-        
+
+            {{-- {{ dd($errors->all()) }} --}}
         
         </div>
     </form>
