6d6c0cdc6ff0c3e34740d0eb65893517551491f4
midgjs <sjqksk.10@gmail.com>
2023-07-19 22:16:02 +0900

    폼 다시 채우기(old 메서드)

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -7,15 +7,16 @@
         <div class="container p-5">
             <h1 class="text-2xl">글쓰기</h1>
             @csrf
-            <input type="text" name="body" class="block w-full mb-2 mt-3 rounded">
+            <input type="text" name="body" class="block w-full mb-2 mt-3 rounded" value="{{ old('body') }}">
             @error('body')
                 <p class="text-xs text-red-500 mb-3"> {{ $message }} </p>
             @enderror
             <button class="py-1 px-3 bg-black text-white rounded text-xs">저장하기</button>
 
             {{-- {{ dd($errors->all()) }} --}}
-        
+            
         </div>
     </form>
+            {{ dd(request()->old('body')) }}
     </body>
 </html>
