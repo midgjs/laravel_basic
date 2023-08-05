f26c196732eb669beb1f526fbeb5958e446d88a4
midgjs <sjqksk.10@gmail.com>
2023-08-05 13:16:11 +0900

    검색

--- app/Http/Controllers/ArticleController.php
+++ app/Http/Controllers/ArticleController.php
@@ -10,6 +10,7 @@ use App\Http\Requests\EditArticleRequest;
 use App\Http\Requests\CreateArticleRequest;
 use App\Http\Requests\DeleteArticleRequest;
 use App\Http\Requests\UpdateArticleRequest;
+use Illuminate\Contracts\Database\Eloquent\Builder;
 
 class ArticleController extends Controller
 {   
@@ -72,13 +73,21 @@ class ArticleController extends Controller
 
     public function index(Request $request) {
         $perPage = $request->input('per_page', 3);
-    
+        
+        $search = $request->input('search');
+
         $articles = Article::with('user') //eagerloading : user모델 접근시 쿼리실행(lazyloading)이 아니고 미리 유저테이블 정보를 한번에 불러온다
             ->withCount('comments')
             ->withExists([
                 'comments as recent_comments_exists' => function($query) {
                     $query->where('created_at', '>', Carbon::now()->subDay());
             }])
+            ->when($search, function($query, $search) {
+                $query->where('body', 'like', "%$search%")
+                ->orWhereHas('user', function(Builder $query) use ($search) {
+                    $query->where('username', 'like', "%$search%");
+                });
+            })
             ->latest()
             ->paginate($perPage)
             ->withQueryString();
@@ -88,6 +97,7 @@ class ArticleController extends Controller
         return view('articles.index', 
         [
             'articles' => $articles,
+            'search' => $search,
         ]);
     
         // return view('articles.index')->with('articles', $articles);
